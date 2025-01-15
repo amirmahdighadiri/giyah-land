a08a36650250857180c31cc2746b8701c76c7300
amirmahdighadiri <168276583+amirmahdighadiri@users.noreply.github.com>
2025-01-15 19:37:28 +0330

    Add files via upload

---
+++ scripts/contact-us.js
@@ -0,0 +1,5 @@
+import {Header} from "../components/header/header.js";
+import {Footer} from "../components/footer/footer.js";
+
+window.customElements.define('header-site', Header)
+window.customElements.define('footer-site', Footer)
