e8eab30b3e9772b490eb5f13c20f499b3128ce9a
amirmahdighadiri <168276583+amirmahdighadiri@users.noreply.github.com>
2025-01-29 21:42:15 +0330

    Delete scripts directory

--- scripts/contact-us.js
+++
@@ -1,5 +0,0 @@
-import {Header} from "../components/header/header.js";
-import {Footer} from "../components/footer/footer.js";
-
-window.customElements.define('header-site', Header)
-window.customElements.define('footer-site', Footer)
